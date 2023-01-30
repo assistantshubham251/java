fc9d3dd2018fe5ceb3e7cf7f6959c5213d8dc0fb
assistantshubham251 <123163520+assistantshubham251@users.noreply.github.com>
2023-01-30 12:37:24 +0530

    Assignment

---
+++ LogicalOperator.java
@@ -0,0 +1,7 @@
+public class LogicalOperator {
+    public static void main(String args[]){
+        System.out.println((5>4) && (8<10));
+        System.out.println((63>67) || (35<65));
+        System.out.println(!(4<5));
+    }
+}
