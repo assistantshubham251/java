ffa1c8572706edd7de8c8a4d8a9eb7820529dcee
assistantshubham251 <123163520+assistantshubham251@users.noreply.github.com>
2023-02-07 13:03:42 +0530

    Delete BitwiseOperator.java

--- BitwiseOperator.java
+++
@@ -1,9 +0,0 @@
-public class BitwiseOperator {
-    public static void main(String args[]){
-        int a=8,b=12;
-        System.out.println("AND" + (a & b));
-        System.out.println("OR" + (a | b));
-        System.out.println("XOR" +(a ^ b));
-        System.out.println("Complement" +(~a));
-    }
-}
