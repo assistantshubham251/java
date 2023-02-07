fa38ad81083fed57c01d4b1c660783f33e8fc3f1
assistantshubham251 <123163520+assistantshubham251@users.noreply.github.com>
2023-02-07 13:27:16 +0530

    Add files via upload

---
+++ Assignment/AssignmentOperator.java
@@ -0,0 +1,16 @@
+public class AssignmentOperator {
+    public static void main(String args[]){
+        int a = 45;
+        a /= 5;
+        System.out.println(a);
+        int b = 7;
+        b *= 8;
+        System.out.println(b);
+        int c = 3;
+        c += 23;
+        System.out.println(c);
+        int d = 80;
+        d -= 10;
+        System.out.println(d);
+    }
+}
