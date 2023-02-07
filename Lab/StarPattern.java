c16cabe0b4198ce17481122b5e65da1fd7c944bd
assistantshubham251 <123163520+assistantshubham251@users.noreply.github.com>
2023-02-07 13:17:50 +0530

    Add files via upload

---
+++ Lab/StarPattern.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class StarPattern{
+    public static void main(String args[]){
+         int n = 8;
+         for (int i=n; i>0; i--){
+            for (int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
+
+
+
+
+
+    }
+}
