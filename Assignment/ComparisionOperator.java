d39a55dab9560994084d21e5a384ee14dfa0e3b9
assistantshubham251 <123163520+assistantshubham251@users.noreply.github.com>
2023-02-07 13:29:05 +0530

    Add files via upload

---
+++ Assignment/ComparisionOperator.java
@@ -0,0 +1,12 @@
+public class ComparisionOperator {
+    public static void main(String args[]){
+        System.out.println(65>45);
+        System.out.println(65<45);
+        System.out.println(8==8);
+        System.out.println(22!=33);
+        System.out.println(4>=5);
+        System.out.println(4<=5);
+    }
+
+}
+
