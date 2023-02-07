54692f93e3b134f38589f1d48403994af6664fa9
assistantshubham251 <123163520+assistantshubham251@users.noreply.github.com>
2023-02-07 13:16:57 +0530

    Add files via upload

---
+++ Lab/Exercise2.java
@@ -0,0 +1,44 @@
+package com.company;
+public class Exercise2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0) {            // the number is divided by 5
+                System.out.println(i);
+                System.out.println(i > 40); // The number is greater than 40
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     }
+
+
+
+
+
+
+}
